0f723f0e5503a1bdd9e3682ee744e1275fe1b98d
wangchao <wangchao@weherepost.com>
2019-08-29 23:01:21 +0800

    增加eureka模块

--- microcloud/microcloud-provider-dept-8001/src/main/java/com/it/Dept_8001_StartSpringCloudApplication.java
+++ microcloud/microcloud-provider-dept-8001/src/main/java/com/it/Dept_8001_StartSpringCloudApplication.java
@@ -3,9 +3,11 @@ package com.it;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan(value = "com.it.dao")
+@EnableEurekaClient
 public class Dept_8001_StartSpringCloudApplication {
 
     public static void main(String[] args) {
