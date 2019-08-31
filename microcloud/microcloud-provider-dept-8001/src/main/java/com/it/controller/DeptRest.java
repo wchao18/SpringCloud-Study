80b66405aee0559cba7a6cae66ffa507faf4475b
wangchao <wangchao@weherepost.com>
2019-08-31 11:35:39 +0800

    打包配置，非执行的jar包不要引用spring-boot-maven-plugin插件

--- microcloud/microcloud-provider-dept-8001/src/main/java/com/it/controller/DeptRest.java
+++ microcloud/microcloud-provider-dept-8001/src/main/java/com/it/controller/DeptRest.java
@@ -3,6 +3,7 @@ package com.it.controller;
 import com.it.service.IDeptService;
 import com.it.vo.Dept;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,16 @@ public class DeptRest {
 
     @Autowired
     private IDeptService deptService;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        return this.discoveryClient;
+    }
+
+
 
     @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
     public Object get(@PathVariable("id") long id) {
