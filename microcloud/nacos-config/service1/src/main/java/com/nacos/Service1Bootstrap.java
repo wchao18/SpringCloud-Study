4b0345edb82890e575463577217d0916f4d9fba1
wangchao <wangchao@weherepost.com>
2020-08-18 18:31:47 +0800

    提交文件

---
+++ microcloud/nacos-config/service1/src/main/java/com/nacos/Service1Bootstrap.java
@@ -0,0 +1,24 @@
+package com.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wangchao
+ * @description TODO
+ * @date 2020/08/17 18:02
+ */
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Service1Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Bootstrap.class,args);
+    }
+
+
+
+
+}
