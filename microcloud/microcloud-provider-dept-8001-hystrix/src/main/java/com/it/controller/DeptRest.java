594563cb25a8e803a60a0900214a773d0e8899b8
wangchao <wangchao@weherepost.com>
2022-04-20 22:10:14 +0800

    升级boot和cloud包

--- microcloud/microcloud-provider-dept-8001-hystrix/src/main/java/com/it/controller/DeptRest.java
+++ microcloud/microcloud-provider-dept-8001-hystrix/src/main/java/com/it/controller/DeptRest.java
@@ -2,14 +2,12 @@ package com.it.controller;
 
 import com.it.service.IDeptService;
 import com.it.vo.Dept;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 
 @RestController
--- microcloud/microcloud-zuul-gateway/src/main/java/com/it/config/IDeptClientZuulServiceFallBackFactory.java
+++ microcloud/microcloud-zuul-gateway/src/main/java/com/it/config/IDeptClientZuulServiceFallBackFactory.java
@@ -1,69 +1,68 @@
-package com.it.config;
-
-import com.alibaba.fastjson.JSONObject;
-import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpResponse;
-import org.springframework.stereotype.Component;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-/**
- * @author wangchao
- * @description 服务降级处理, 服务不可用的情况下调用
- * @date 2019/09/02 14:02
- */
-@Component
-public class IDeptClientZuulServiceFallBackFactory implements ZuulFallbackProvider {
-
-
-    @Override
-    public String getRoute() {
-        return "*"; //*代表所有的服务都支持回退
-    }
-
-    @Override
-    public ClientHttpResponse fallbackResponse() {
-        return new ClientHttpResponse() {
-            @Override
-            public HttpStatus getStatusCode() throws IOException {
-                return HttpStatus.BAD_REQUEST;
-            }
-
-            @Override
-            public int getRawStatusCode() throws IOException {
-                return HttpStatus.BAD_REQUEST.value();
-            }
-
-            @Override
-            public String getStatusText() throws IOException {
-                return HttpStatus.BAD_REQUEST.getReasonPhrase();
-            }
-
-            @Override
-            public void close() {
-
-            }
-
-            @Override
-            public InputStream getBody() throws IOException {
-                JSONObject r = new JSONObject();
-                r.put("state", "9999");
-                r.put("msg", "系统错误，请求失败");
-                return new ByteArrayInputStream(r.toJSONString().getBytes("UTF-8"));
-            }
-
-            @Override
-            public HttpHeaders getHeaders() {
-                HttpHeaders httpHeaders = new HttpHeaders();
-                httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
-                //httpHeaders.set("Content-Type", "text/html; charset=UTF-8");
-                return httpHeaders;
-            }
-        };
-    }
-}
+//package com.it.config;
+//
+//import com.alibaba.fastjson.JSONObject;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.MediaType;
+//import org.springframework.http.client.ClientHttpResponse;
+//import org.springframework.stereotype.Component;
+//
+//import java.io.ByteArrayInputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//
+///**
+// * @author wangchao
+// * @description 服务降级处理, 服务不可用的情况下调用
+// * @date 2019/09/02 14:02
+// */
+//@Component
+//public class IDeptClientZuulServiceFallBackFactory implements ZuulFallbackProvider {
+//
+//
+//    @Override
+//    public String getRoute() {
+//        return "*"; //*代表所有的服务都支持回退
+//    }
+//
+//    @Override
+//    public ClientHttpResponse fallbackResponse() {
+//        return new ClientHttpResponse() {
+//            @Override
+//            public HttpStatus getStatusCode() throws IOException {
+//                return HttpStatus.BAD_REQUEST;
+//            }
+//
+//            @Override
+//            public int getRawStatusCode() throws IOException {
+//                return HttpStatus.BAD_REQUEST.value();
+//            }
+//
+//            @Override
+//            public String getStatusText() throws IOException {
+//                return HttpStatus.BAD_REQUEST.getReasonPhrase();
+//            }
+//
+//            @Override
+//            public void close() {
+//
+//            }
+//
+//            @Override
+//            public InputStream getBody() throws IOException {
+//                JSONObject r = new JSONObject();
+//                r.put("state", "9999");
+//                r.put("msg", "系统错误，请求失败");
+//                return new ByteArrayInputStream(r.toJSONString().getBytes("UTF-8"));
+//            }
+//
+//            @Override
+//            public HttpHeaders getHeaders() {
+//                HttpHeaders httpHeaders = new HttpHeaders();
+//                httpHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+//                //httpHeaders.set("Content-Type", "text/html; charset=UTF-8");
+//                return httpHeaders;
+//            }
+//        };
+//    }
+//}
